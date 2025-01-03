08519f007fa72bdf492cd0ad34cfde8d6217d324
nicodiaz <ngdiaz00@gmailcom>
2025-01-03 09:01:57 -0300

    Se crea Clase Articulo

---
+++ TPWeb_equipo-J/Dominio/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id {  get; set; }
+        public string CodigoArticulo {  get; set; }
+
+        public string DescripcionArticulo { get; set; }
+
+        public Marca Marca { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public Decimal Precio {  get; set; } 
+    }
+}
