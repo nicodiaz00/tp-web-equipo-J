424f58718e46065040de32d111e9409a1a1ef0c0
nico <ngdiaz00@gmail.com>
2025-01-07 10:16:09 -0300

    Se crea logica listar clientes

--- TPWeb_equipo-J/Negocio/ClienteNegocio.cs
+++ TPWeb_equipo-J/Negocio/ClienteNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace Negocio
 {
-    internal class ClienteNegocio
+    public class ClienteNegocio
     {
+        public List<Cliente> listarClientes()
+        {
+            List<Cliente> listaClientes = new List<Cliente>();
+            AccesoDatos accesoDatos = new AccesoDatos();
+
+            try
+            {
+                accesoDatos.setearConsulta("Select Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP from Clientes");
+                accesoDatos.ejecutarLectura();
+
+                while (accesoDatos.Lector.Read())
+                {
+                    Cliente cliente = new Cliente();
+                    cliente.Id = (int)accesoDatos.Lector["Id"];
+                    cliente.Dni = (string)accesoDatos.Lector["Documento"];
+                    cliente.Nombre = (string)accesoDatos.Lector["Nombre"];
+                    cliente.Apellido = (string)accesoDatos.Lector["Apellido"];
+                    cliente.Email = (string)accesoDatos.Lector["Email"];
+                    cliente.Direccion = (string)accesoDatos.Lector["Direccion"];
+                    cliente.Ciudad = (string)accesoDatos.Lector["Ciudad"];
+                    cliente.Cp = (int)accesoDatos.Lector["CP"];
+
+                    listaClientes.Add(cliente);
+                }
+                return listaClientes;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
     }
 }
